c692f06cf66bf4b7b5532cea93e63bf6678b321a
Shrikant Kushwah <96473205+shrikant-kushwah@users.noreply.github.com>
2023-04-23 19:47:44 +0530

    Add files via upload

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        A credit card is a financial instrument issued by banks with a pre-set credit limit, helping you make cashless transactions. The card issuer determines the credit limit based on your credit score, credit history and your income.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
